c9bf96b482dc1cb2976e2411425642efccab80c3
LorenaMayrink <franklinmlorena@gmail.com>
2022-07-12 21:08:57 -0300

    exs modulo]

---
+++ Exercícios/modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraHobby, mostraIdade} from "./funcoes.mjs";
+
+console.log(mostraIdade("Lorena", 22));
+console.log(mostraCidade("Lorena", "São Carlos"));
+console.log(mostraHobby("Lorena", "Ler livros"));
